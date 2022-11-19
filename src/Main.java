9155c9bc13a754e2209aee3a1e4f6c4e2270cd15
PCH <mr.piotrchmura@gmail.om>
2022-11-19 12:19:18 +0100

    Day 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
 
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 4;
+        double b = 4.0;
+        String c = "HackerRank";
+
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        scanner.nextLine();
+        String s = scanner.nextLine();
+        System.out.println(a+i);
+        System.out.println(b+d);
+        System.out.println(c+" "+s);
 
     }
 }
