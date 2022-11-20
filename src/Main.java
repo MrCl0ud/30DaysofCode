e7c8ec514b8b69f6bccee721a1e95427ce91d972
PCH <mr.piotrchmura@gmail.com>
2022-11-20 10:41:04 +0100

    Day 2

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        solve(12.0,20,8);
 
-        int a = 4;
-        double b = 4.0;
-        String c = "HackerRank";
-
-        int i = scanner.nextInt();
-        double d = scanner.nextDouble();
-        scanner.nextLine();
-        String s = scanner.nextLine();
-        System.out.println(a+i);
-        System.out.println(b+d);
-        System.out.println(c+" "+s);
-
+    }
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        // Write your code here
+        double total_cost = 0;
+        double tip = (tip_percent * meal_cost) /100;
+        double tax = (tax_percent * meal_cost) /100;
+        total_cost = meal_cost + tip + tax;
+        System.out.println(Math.round(total_cost));
     }
 }
