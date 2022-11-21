868139541a2ed8c59c8464aa89fb85c8ac79f631
PCH <mr.piotrchmura@gmail.com>
2022-11-21 08:20:45 +0100

    Day 3

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        solve(12.0,20,8);
-
-    }
-    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        // Write your code here
-        double total_cost = 0;
-        double tip = (tip_percent * meal_cost) /100;
-        double tax = (tax_percent * meal_cost) /100;
-        total_cost = meal_cost + tip + tax;
-        System.out.println(Math.round(total_cost));
+        int n = 5;
+        if (n % 2 == 0) {
+            if ((n <= 5) && (n >= 2)) System.out.println("Not Weird");
+            if ((n > 5) && (n <= 20)) System.out.println("Weird");
+            if (n > 20) System.out.println("Not Weird");
+        }
+        else System.out.println("Weird");
     }
 }
