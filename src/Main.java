576e0930c57e36c4d284b88f2d660561d0a3ae6c
PCH <mr.piotrchmura@gmail.com>
2022-11-23 19:19:40 +0100

    Day 5

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 5;
-        if (n % 2 == 0) {
-            if ((n <= 5) && (n >= 2)) System.out.println("Not Weird");
-            if ((n > 5) && (n <= 20)) System.out.println("Weird");
-            if (n > 20) System.out.println("Not Weird");
-        }
-        else System.out.println("Weird");
+        Person person = new Person(5);
+        person.amIOld();
     }
 }
