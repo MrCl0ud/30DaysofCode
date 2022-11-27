3969304d8c5c1b060ebcf25e11c60f2a25b6c1dc
PCH <mr.piotrchmura@gmail.om>
2022-11-27 17:15:30 +0100

    Day 7

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        int a = 4;
-        double b = 4.0;
-        String c = "HackerRank";
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
 
-        int i = scanner.nextInt();
-        double d = scanner.nextDouble();
-        scanner.nextLine();
-        String s = scanner.nextLine();
-        System.out.println(a+i);
-        System.out.println(b+d);
-        System.out.println(c+" "+s);
+        Collections.reverse(arr);
 
+        for (int i = 0; i < n; i++){
+            System.out.print(arr.get(i) + " ");
+        }
+
+        bufferedReader.close();
     }
 }
